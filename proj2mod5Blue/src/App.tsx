013b6cfa2d4879d10c1ded9d6f41f4f3b2c36445
maxxcleiton <maxcleiton@live.com>
2022-10-26 15:30:22 -0300

    vite, material ui instalados e menu adicionado

---
+++ proj2mod5Blue/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+function App() {
+
+  return (
+    <main className="">
+        <ul className="horizontal-menu">
+          <li>Cadastrar</li>
+          <li>Logar</li>
+          <li>Adicionar</li>
+        </ul>
+    </main>
+  )
+}
+
+export default App
